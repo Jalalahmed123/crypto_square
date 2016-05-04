bf32f3fec233f3eb659d3486b2ed322a965a9c89
Jalalahmed123 <ahmedjx@dukes.jmu.edu>
2016-05-04 01:48:58 -0400

    Update crypto-square.js

--- crypto-square.js
+++ crypto-square.js
@@ -1,6 +1,6 @@
 // create our Crypto class
 // it takes a string as an argument
-var Crypto = function(text) {
+var Crypto = function(text){
 	this.message = text;
 };
 
@@ -9,42 +9,32 @@ Crypto.prototype.normalizePlaintext = function() {
 };
 
 Crypto.prototype.size = function() {
-	var length = this.normalizePlaintext().length;
-	return Math.ceil(Math.sqrt(length));
+	var length = this.normalizePlainText().length;
+	return Math.ceil(Math.sqrt(length));		
 };
 
 Crypto.prototype.plaintextSegments = function() {
 	var segments = [],
 		npt = this.normalizePlaintext(),
 		size = this.size();
-
 	for (var i = 0; i < npt.length; i += size) {
-		// extract segments from the normalized plain text
-		// and add it to the segements array
-		segments.push(npt.substr(i, size));
+		segments.push(npt.substr(i,size));
 	}
-
-	// return an array of strings representing
-	// segments of the normalized plain text
+	//return an array
 	return segments;
 };
 
 Crypto.prototype.ciphertext = function() {
-	var ct = "",
-		size = this.size(), // the number of columns
-		segs = this.plaintextSegments(); // the rows
-
-	// loop through the columns
+	var code = "",
+		size = this.size(),
+		segs = this.plaintextSegments();
 	for (var i = 0; i < size; i += 1) {
-		// loop through the rows
 		for (var j = 0; j < segs.length; j += 1) {
-			ct += segs[j].charAt(i);
+			code += segs[j].charAt(i);
 		}
 	}
-
-	// returns a string that represents
-	// the cipher text, i.e. the encoded message
-	return ct;
+	//return a string
+	return code;
 };
 
 module.exports = Crypto;
